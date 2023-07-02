72ebcc5b85c902e12642014881752e242c1aa9a5
Rockantium <99689755+Rockantium@users.noreply.github.com>
2023-07-01 23:03:28 -0500

    main.py

--- main.py
+++ main.py
@@ -2,32 +2,44 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-def make_stronger_password(password):
-  """Makes a password stronger by replacing characters and appending "q*s" to the end.
+def password_modifier(password):
+  """Modifies a password by replacing certain characters and appending "q*s" to the end.
 
   Args:
-    password: The password to make stronger.
+    password: The password to be modified.
 
   Returns:
-    The stronger password.
+    The modified password.
   """
 
-  password = password.replace("i", "!")
-  password = password.replace("a", "@")
-  password = password.replace("m", "M")
-  password = password.replace("B", "8")
-  password = password.replace("o", ".")
-  return password + "q*s"
+  modified_password = ""
+  for char in password:
+    if char == "i":
+      modified_password += "!"
+    elif char == "a":
+      modified_password += "@"
+    elif char == "m":
+      modified_password += "M"
+    elif char == "B":
+      modified_password += "8"
+    elif char == "o":
+      modified_password += "."
+    else:
+      modified_password += char
+
+  modified_password += "q*s"
+  return modified_password
 
 
 def main():
+  """Prompts the user for a password and then prints the modified password."""
+
   password = input("Enter a password: ")
-  stronger_password = make_stronger_password(password)
-  print(f"The stronger password is: {stronger_password}")
+  modified_password = password_modifier(password)
+  print(f"The modified password is: {modified_password}")
 
 
 if __name__ == "__main__":
   main()
-
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
